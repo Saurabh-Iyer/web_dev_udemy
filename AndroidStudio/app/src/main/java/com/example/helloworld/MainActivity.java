3c37043ad1ef9f17007c120cd931e3132014a0a3
Saurabh Iyer <saurabhiyer1999@gmail.com>
2021-03-04 19:14:31 +0530

    Android Studio

---
+++ AndroidStudio/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void onBtnClick (View view){
+
+        TextView txtHello = findViewById(R.id.mainText);
+        EditText edtTxtName = findViewById(R.id.edtTxtName);
+        txtHello.setText("Hello  "+ edtTxtName.getText().toString());
+
+    }
+}
